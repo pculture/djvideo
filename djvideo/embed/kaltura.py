4b88c371e11b56808f370e7734e0eddebe79ae39
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-04-19 19:36:20 -0700

    Added support for Kaltura embeds.

--- djvideo/embed/kaltura.py
+++ djvideo/embed/kaltura.py
@@ -23,5 +23,38 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from djvideo.embed.base import registry, EmbedGenerator
-from djvideo.embed import youtube, vimeo, mp3, files, blip, kaltura
+import re
+
+from vidscraper.exceptions import UnhandledVideo
+
+from djvideo.embed import EmbedGenerator, registry
+
+
+class KalturaEmbedGenerator(EmbedGenerator):
+    path_re = re.compile(r'^https?://cdnsecakmi.kaltura.com/p/'
+                         r'(?P<partner_id>[\d]+)/sp/(?P<subp_id>[\d]+)'
+                         r'/flvclipper/entry_id/(?P<entry_id>[\w]+)/'
+                         r'version/0(?:/ext/flv)?')
+    template = 'djvideo/kaltura.html'
+    default_context = {'width': 459, 'height': 344}
+
+    def get_url_data(self, url):
+        match = self.path_re.match(url)
+        if match:
+            return match.groupdict()
+        raise UnhandledVideo(url)
+
+    def get_context(self, url, context):
+        c = super(KalturaEmbedGenerator, self).get_context(url, context)
+        c.update(self.get_url_data(url))
+        return c
+
+    def handles_video_url(self, url):
+        try:
+            self.get_url_data(url)
+        except UnhandledVideo:
+            return False
+        return True
+
+
+registry.register(KalturaEmbedGenerator)
